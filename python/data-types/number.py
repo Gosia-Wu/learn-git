460f578342f2c46b9a8e4a8fe9671190236c7ca6
Malgorzata <tinu@wp.pl>
2024-08-09 13:19:47 +0200

    add: number.py

---
+++ python/data-types/number.py
@@ -0,0 +1,16 @@
+# Typy danych - number (liczby).
+
+# Rozróżniamy 3 rodzaj liczb:
+
+# 1) int - 1
+# 2) float - 2.5 (liczba dziesiętna)
+# 3) complex - 3j (liczba z tkzw suffix'em)
+
+# value = 3j
+
+# print(value))
+
+
+# Przykład importowania modułu(paczki - random), która pozwala na wylosuje randomowej (losowej)liczby z zadanego(określonego) zakresu.
+# import random ( Wybieramy liczbę losową z zakresu od 0 do 1000000)
+# print(random.randrange(0, 1000000))
