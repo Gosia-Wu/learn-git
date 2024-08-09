1b988b2bcba59e41bb8a391f50c82c3fc81019dd
Malgorzata <tinu@wp.pl>
2024-08-09 14:39:29 +0200

    add: tuples.py

---
+++ python/data-types/tuples.py
@@ -0,0 +1,5 @@
+# tupa - zbiór danych, w którym magazynujemy różne typy danych, gdzie nie możemy zmienić
+# ani usunąć kolejności ani wartości żadnego z tych elementów
+
+# thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+# print(thistuple)
