59f94724574551a5de20d56335a04ecc23a961b8
Malgorzata <tinu@wp.pl>
2024-08-09 12:01:57 +0200

    add: string.py

---
+++ python/data-types/string.py
@@ -0,0 +1,37 @@
+person = "Łukasz Kwiecień"
+# print(person)
+
+# konsologowanie każdej poszczególnej wartości string w ramach jego indeksu
+# print(person[0])
+# print(person[1])
+# print(person[2])
+# print(person[3])
+# print(person[4])
+# print(person[5])
+# print(person[6])
+# print(person[7])
+# print(person[8])
+# print(person[9])
+# print(person[10])
+# print(person[11])
+# print(person[12])
+# print(person[13])
+# print(person[14])
+
+# Pętla - iterowanie po każdym indeksie string'a
+
+# for letterOfPerson in person:
+#     print(letterOfPerson)
+
+# len - sprawdzanie długości string'a
+
+# print(len(person))
+
+# sprawdzanie istnienia frazy w całym ciągu znaków
+
+# print("Kwie" in person)
+
+# Instrukcja warunkowa if dla string'a
+
+# if "Kwie" in person:
+#     print("Tak, jest 'Kwie'")
