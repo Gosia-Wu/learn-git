0fb191275e03460d8ea59bd0cb77c68009752186
Malgorzata <tinu@wp.pl>
2024-08-09 14:25:42 +0200

    add: list.py

---
+++ python/data-types/list.py
@@ -0,0 +1,38 @@
+# List - lista to tak naprawdę tablica elementów, kt
+
+# list = ["Łukasz", "Gosia", "Janek", "Rafał", "Aniela", "Gosia", "Łukasz"]
+# print(list)
+
+#Dostęp do danej grupy elementów bazując na indeksach
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:5])
+
+#Nadpisanie wartości elementu listy na inny
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
+
+#append - dodawanie nowego elementu do listy. Nowy element jest zawsze dodawany na końcu listy.
+
+# furnitureList = ["table","chair", "bed"]
+# furnitureList.append("wardrobe")
+# print(furnitureList)
+
+#remove - usunięcie istniejącego elementu z listy.
+
+# furnitureList = ["table","chair", "bed"]
+# furnitureList.remove("chair")
+# print(furnitureList)
+
+# Pętla po liście
+
+# thislist = ["apple", "banana", "cherry"]
+# for i in thislist:
+#   print(i) 
+
+# sugar syntax (skrócony zapis) pętli po liście
+
+# thislist = ["apple", "banana", "cherry"]
+# [print(x) for x in thislist] 
