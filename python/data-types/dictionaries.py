205f828e7eabe2b7b4ec46e6b66e1e1dab3c05d0
Malgorzata <tinu@wp.pl>
2024-08-10 07:16:08 +0200

    add: dictionaries.py

---
+++ python/data-types/dictionaries.py
@@ -0,0 +1,15 @@
+# Słowniki - to kolekcje, w których przechowujemy dane w parach: klucz : wartosść
+# Słownik jest uporzadkowany (od Python ver.3,7), pozwala na zmiany i nie pozwala na duplikaty
+# (klucze muszą być unikatowe, gdyż wartosść przypisana do klucza, który był już wcześniej sparowany
+# z jakąś wartością, nadpisze <zastąpi> dotychczasową wartość)
+
+# thisdict =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(thisdict)
+
+# Używając nazwy klucza możemy wywołać jego wartość
+
+# print(thisdict["brand"])
