8b65d41bd2c37fbe3d766cbd9c28afe1c9f0a623
Malgorzata <tinu@wp.pl>
2024-08-11 20:17:10 +0200

    Zadanie 15: Tworzenie funkcji

---
+++ python-zadania/Zadanie-15.py
@@ -0,0 +1,14 @@
+# Zadanie 15: Tworzenie funkcji
+
+# Utwórz funkcję sum_list, która przyjmuje listę liczb i zwraca ich sumę.
+#  Przetestuj tę funkcję na przykładowej liście.
+
+# definiuję przykładową listę
+NumbersList = [14, 25, 9, 37, 6]
+
+# tworzę funkcję
+def sum_list():
+    print(sum(NumbersList))
+
+# wywoluję funkcję
+sum_list()
