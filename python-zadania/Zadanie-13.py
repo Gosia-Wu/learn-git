c027239a182d99cb2c841afe4fca23189cf58348
Malgorzata <tinu@wp.pl>
2024-08-11 17:50:23 +0200

    Zadanie 13: Zagnieżdżone listy i słowniki

---
+++ python-zadania/Zadanie-13.py
@@ -0,0 +1,12 @@
+# Zadanie 13: Zagnieżdżone listy i słowniki
+
+# Utwórz listę zawierającą trzy słowniki, z których każdy będzie miał klucze "name" i "age".
+#  Wypełnij je odpowiednimi wartościami i wydrukuj całą listę.
+
+
+listOfDictionaries = [
+    {"name" : "Barbara", "age" : 27},
+    {"name" : "Paweł", "age" : 35},
+    {"name" : "Hanna", "age" : 31}   
+]
+print(listOfDictionaries)
