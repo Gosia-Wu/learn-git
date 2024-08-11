551294d5b6d79ad965eebf6f154b7af942987464
Malgorzata <tinu@wp.pl>
2024-08-11 16:13:20 +0200

    Zadanie 11: Konwersja listy na tuple.

---
+++ python-zadania/Zadanie-11.py
@@ -0,0 +1,9 @@
+# Zadanie 11: Konwersja listy na tuple.
+# Utwórz listę zawierającą trzy liczby całkowite. Następnie przekształć tę listę na tuple
+#  i wydrukuj wynik.
+
+integersList = [674, 389, -521]
+print(integersList)
+
+integersTuple = tuple(integersList)
+print(integersTuple)
