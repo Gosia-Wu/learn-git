2571c8b42dbb23dbbd51336f48f63694754dcfe5
Malgorzata <tinu@wp.pl>
2024-08-11 13:25:18 +0200

    Zadanie 10: Konwersja listy na set

---
+++ python-zadania/Zadanie-10.py
@@ -0,0 +1,9 @@
+# Zadanie 10: Konwersja listy na set
+# Utwórz listę z powtarzającymi się wartościami (np. [1, 2, 2, 3, 3, 3]). 
+# Przekształć tę listę na set i wydrukuj wynik.
+
+valueList = [1, 2, 2, 3, 3, 3]
+print(valueList)
+
+valueSet = set(valueList)
+print(valueSet)
